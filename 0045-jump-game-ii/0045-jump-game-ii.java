00b04e0510edb967ba1476112348b37e7d0d50c8
vaibhavmeshram127 <vaibhavmeshram127@gmail.com>
2026-07-04 14:33:12 +0530

    Time: 2 ms (31.71%), Space: 47.6 MB (8.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -9,7 +9,7 @@ class Solution {
         int curr=0;
         int max=0;
 
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length-1;i++){
             max=Math.max(max,nums[i]+i);
 
             if(i==curr){
