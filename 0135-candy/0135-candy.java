d7d1ee1952ce43bbadff9e19818c1db58b02d655
vaibhavmeshram127 <vaibhavmeshram127@gmail.com>
2026-07-04 16:36:15 +0530

    Time: 3 ms (88.8%), Space: 47.7 MB (51%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length==1){
+            return 1;
+        }
+        int[] candy=new int[ratings.length];
+        for(int i=0;i<ratings.length;i++){
+            candy[i]=1;
+        }
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]){
+                candy[i]=candy[i-1]+1;
+            }
+        }
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candy[i]=Math.max(candy[i], candy[i+1] + 1);
+            }
+        }
+        int min=0;
+        for(int i=0;i<candy.length;i++){
+            min+=candy[i];
+            
+        }
+        return min;
+
+    }
+}
