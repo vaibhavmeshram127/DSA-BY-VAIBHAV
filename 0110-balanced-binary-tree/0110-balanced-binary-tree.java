90d2050575e6f0ce3030fb98ab2b4fddd3ebde61
vaibhavmeshram127 <vaibhavmeshram127@gmail.com>
2026-09-16 19:05:48 +0530

    Time: 0 ms (100%), Space: 45.6 MB (41.92%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -18,34 +18,38 @@ class Solution {
         if(root==null){
             return true;
         }
-        int ans=length(root);
+        int ans=helper(root);
         if(ans==-1){
             return false;
         }
         return true;
+        
     }
-    public int length(TreeNode root){
+    public int helper(TreeNode root){
         if(root==null){
+
             return 0;
         }
 
-
-        int left=length(root.left);
+        int left=helper(root.left);
         if(left==-1){
             return -1;
         }
-        int right=length(root.right);
+
+        int right=helper(root.right);
         if(right==-1){
             return -1;
         }
+
         if(Math.abs(left-right)>1){
             return -1;
         }
         return Math.max(left,right)+1;
+       
+       
+
         
 
-
-
-
     }
+
 }
